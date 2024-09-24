034e5666867cc8fa5d5b6ae230603b77fc4474a8
Eng.\ Tharwat El-Sayed <tharwat_uss89@hotmail.com>
2024-09-24 19:38:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ if selected == "Home":
     the count of hate speech and offensive language identifications, and a class label indicating 
     the majority opinion: 0 for hate speech, 1 for offensive language, and 2 for neither.\n
     The dataset published in:\n
-    Davidson, T., Warmsley, D., Macy, M., & Weber, I. (2017, May). Automated hate speech detection 
-    and the problem of offensive language. In Proceedings of the international AAAI conference on 
-    web and social media (Vol. 11, No. 1, pp. 512-515).
+    Davidson, T., Warmsley, D., Macy, M., & Weber, I. (2017, May). Automated hate speech 
+    detection and the problem of offensive language. In Proceedings of the international 
+    AAAI conference on web and social media (Vol. 11, No. 1, pp. 512-515).
     The Dataset can be downloaded from:
     
     https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset
