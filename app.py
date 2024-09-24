8e2441207bc753bb62a406acc164f26c1a9315b6
Eng.\ Tharwat El-Sayed <tharwat_uss89@hotmail.com>
2024-09-24 14:33:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -96,37 +96,6 @@ elif chart_type == 'Pie Chart':
     plt.title('Distribution of Classes')
     st.pyplot(plt)
 
-# Horizontal line separator
-st.markdown("---")
-
-st.subheader('Understanding Class Distribution')
-
-# Create tabs
-tab1, tab2 = st.tabs(["Tweets Before Preprocessing", "Tweets After Stemming"])
-
-# Tab 1: Dataset Brief Information
-with tab1:
-    st.subheader('Distribution of Classes (Bar Chart)')
-    plt.figure(figsize=(8, 6))
-    plt.hist(df['class'], bins=3, edgecolor='black')
-    plt.xlabel('Class')
-    plt.ylabel('Frequency')
-    plt.title('Distribution of Classes')
-    plt.xticks([0, 1, 2], ['Hate Speech', 'Offensive Language', 'Neither'])
-    st.pyplot(plt)
- 
-# Tab 2: Dataset Columns Description
-with tab2:
-    st.subheader('Proportion of Classes (Pie Chart)')
-    labels = 'Hate Speech', 'Offensive Language', 'Neither'
-    sizes = df['class'].value_counts()
-    explode = (0, 0.1, 0)  # only "explode" the 2nd slice
-    fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
-    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    plt.title('Distribution of Classes')
-    st.pyplot(plt)
 
 # Horizontal line separator
 st.markdown("---")
